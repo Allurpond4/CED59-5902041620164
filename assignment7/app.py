e9aa81da3b6551665cb786b67526cc0968a5b3d0
Allurpond4 <paitoon_love@hotmail.com>
2019-08-08 15:58:18 +0700

    gg

--- assignment7/app.py
+++ assignment7/app.py
@@ -1,18 +1,19 @@
-from flask import Flask,render_template
-
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtfforms.validators import DataRequired
 
 app = Flask(__name__)
 
 
-@app.route('/',method=["post"])
-
+@app.route('/', method=["post"])
 def index():
     return render_template('form1.html')
+
 
 def register():
     return 'Welcome to my world'
 
 
 if __name__ == '__main__':
-
-  app.run()
+    app.run()
